01b270c8eb6ff805cfc50eca668dd89bdf1a8180
Kieran Litschel <s1614973@gasquet.inf.ed.ac.uk>
2018-11-17 19:47:12 +0000

    Complete method to find closest together using contor and hull difference

--- find_smallest_hull_difference.py
+++ find_smallest_hull_difference.py
@@ -1,42 +1,41 @@
 import cv2
 import os
 import numpy as np
+from matplotlib import pyplot as plt
 dirname = os.path.dirname(__file__)
 
 
 def find_diff(valid,invalid):
-    mask = np.concatenate((valid, invalid), axis=0)
 
-    im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    im2, contours_valid, hierarchy = cv2.findContours(valid,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    im2, contours_invalid, hierarchy = cv2.findContours(invalid,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    dc = cv2.convexHull(contours[0])
-    areadiff1 = cv2.contourArea(contours[0]) - cv2.contourArea(dc)
-    dc2 = cv2.convexHull(contours[1])
-    areadiff2 = cv2.contourArea(contours[1]) - cv2.contourArea(dc2)
+    dc = cv2.convexHull(contours_valid[0])
+    areadiff1 = cv2.contourArea(contours_valid[0]) - cv2.contourArea(dc)
+    dc2 = cv2.convexHull(contours_invalid[0])
+    areadiff2 = cv2.contourArea(contours_invalid[0]) - cv2.contourArea(dc2)
 
-    print("Area diffs %s and %s respectively"
-            %(areadiff1,areadiff2))
-
-    print("%s contours"%len(contours))
-
-    return abs(areadiff2-areadiff1), areadiff1, areadiff2
+    return abs(areadiff1)-abs(areadiff2), areadiff1, areadiff2
 
 valids = []
 invalids = []
 
 for valid_num in range(1,155):
     valid = cv2.imread("/afs/inf.ed.ac.uk/user/s16/s1614973/Documents/IVR/IVR-assignment/valid/validxy%s.jpg"%valid_num,0)
-    valids.append(valid)
+    ret,thresh = cv2.threshold(valid,127,255,cv2.THRESH_BINARY)
+    valids.append(thresh)
 
 for invalid_num in range(1,157):
     invalid = cv2.imread("/afs/inf.ed.ac.uk/user/s16/s1614973/Documents/IVR/IVR-assignment/invalid/invalidxy%s.jpg"%invalid_num,0)
-    invalids.append(invalid)
+    ret,thresh = cv2.threshold(invalid,127,255,cv2.THRESH_BINARY)
+    invalids.append(thresh)
 
 smallestDiff = float('inf')
 validNo = -1
 validAreaDiff = 0
 invalidNo = -1
 invalidAreaDiff = 0
+diffs = []
 
 i = 1
 for valid in valids:
@@ -51,8 +50,15 @@ for valid in valids:
             validAreaDiff = area_valid
             invalidAreaDiff = area_invalid
             smallestDiff = area_diffs
+        diffs.append(area_diffs)
         j+=1
     i+=1
+1
+plt.hist(diffs,10)
+plt.title("Histogram of how much greater difference between contour area and convex hull area is for each valid target compared to invalid")
+plt.xlabel("Difference between valid and invalid targets difference between contour area and convex hull area")
+plt.ylabel("Frequency")
+plt.show()
 
 print("Smallest diff is %s with valid %s and invalid %s with area diffs %s and %s respectively"
         %(smallestDiff,validNo,invalidNo,validAreaDiff,invalidAreaDiff))
